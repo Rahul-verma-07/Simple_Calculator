096ed879334a8ca8df3e182e32a8af9c46c8cc17
RV <rv893302@gmail.com>
2026-01-15 23:11:51 +0530

    Add basic operants and operations

---
+++ Code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+
+int main()
+{
+    float n1, n2;
+    char op, again;
+    
+    printf("\n=== SIMPLE CALCULATOR ===\n");
+    printf("Operations: +  -  *  /  ^  s(sqrt)  c(cbrt)\n");
+    printf("Examples:  3+4   |   2^5   |   25s  |   125c\n\n");
+
+    return 0;
+}

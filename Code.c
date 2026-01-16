f92a0a45fdfe478c52f0fab938f94c33a4cfe8ae
RV <rv893302@gmail.com>
2026-01-16 22:49:19 +0530

    Defined basic_operation function

--- Code.c
+++ Code.c
@@ -10,7 +10,7 @@ int main()
 {
     float num1, num2;
     char op, again;
-    
+
     printf("\n=== SIMPLE CALCULATOR ===\n");
     printf("Operations: +  -  *  /  ^  s(sqrt)  c(cbrt)\n");
     printf("Examples:  3+4   |   2^5   |   25s  |   125c\n\n");
@@ -19,7 +19,7 @@ int main()
     {
         printf("Enter your Expression:  ");
 
-        //Read first number
+        // Read first number
         if (scanf("%f", &num1) != 1)
         {
             printf("Invalid Input!\n");
@@ -27,19 +27,22 @@ int main()
             return 1;
         }
 
-        //Read operations
+        // Read operations
         scanf(" %c", &op);
 
-        //Read second operant
+        // Read second operant
         if (op != 's' && op != 'c')
         {
             scanf("%f", &num2);
         }
-        
-        //Calculations
+
+        // Calculations
         switch (op)
         {
-        case '+': case '-': case '*': case '/':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
             basic_operation(num1, num2, op);
             break;
 
@@ -54,14 +57,14 @@ int main()
         case 'c':
             handle_root(num1, op);
             break;
-        
+
         default:
             printf("Error: Unknown operator '%c'\n", op);
             break;
         }
 
-        printf("Do you want to exit (y/n): ");
-        scanf(" %c",&again);
+        printf("\nDo you want to exit (y/n): ");
+        scanf(" %c", &again);
         again = towlower(again);
 
         if (again != 'y' && again != 'n')
@@ -69,11 +72,40 @@ int main()
             printf("Invalid Input! Exiting...\n");
             return 1;
         }
-        
+
         printf("\n");
 
     } while (1);
-    
-    printf("Goodbye!\n");
+
+    printf("Goodbye..!\n");
     return 0;
+}
+
+void basic_operation(float n1, float n2, char op)
+{
+    if (op == '/' && n2 == 0)
+    {
+        printf("Error: Division by zero!");
+        return;
+    }
+
+    float result;
+
+    switch (op)
+    {
+    case '+':
+        result = n1 + n2;
+        break;
+    case '-':
+        result = n1 - n2;
+        break;
+    case '*':
+        result = n1 * n2;
+        break;
+    case '/':
+        result = n1 / n2;
+        break;
+    }
+
+    printf("%g %c %g = %g", n1, op, n2, result);
 }
